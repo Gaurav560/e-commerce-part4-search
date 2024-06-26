5e9b49dcfc9012776b4c18aeeec72b2b17537bbe
Gaurav Sharma <gsharma1154@gmail.com>
2024-06-26 21:58:39 +0530

    implemented search functionality

---
+++ src/main/java/com/telusko/ECommercePart4SearchApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePart4SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePart4SearchApplication.class, args);
+    }
+
+}
